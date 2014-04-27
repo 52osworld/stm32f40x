c314c4ddc09ca56e63155b923acd6d0a1046d562
shavvn <tig314@gmail.com>
2014-04-27 16:23:08 +0800

    add comments
    add comments

--- applications/application.c
+++ applications/application.c
@@ -30,31 +30,39 @@
 #include "stm32_eth.h"
 #endif
 
-static struct rt_messagequeue  rx_mq;
-static char msg_pool[1024]; 
+//global variables
+static struct rt_messagequeue  uart_1_rx_mq;  //message queue
+static char msg_pool[1024]; //message pool, size 1024
+
+//self-defined message struct
 struct  rx_msg
 {
-  rt_device_t dev;
-  rt_size_t size;
+  rt_device_t dev;  //received data from dev
+  rt_size_t size;  //received data size
 };
 
-
-rt_err_t uart_input_proc(rt_device_t dev, rt_size_t size) {
+/***************local funcions***************/
+//@fn: uart_input_proc
+//@brief: call back function of uart rx, then send a msg to message queue
+//@para: dev: device
+//@para: size: size of data received
+//@return: RT_EOK if succeedd
+static rt_err_t uart_input_proc(rt_device_t dev, rt_size_t size) {
   struct rx_msg msg;
   msg.dev = dev;
   msg.size = size;
-  return rt_mq_send(&rx_mq, &msg, sizeof(struct rx_msg));
+  //send a message to queue with device info and size of data received
+  return rt_mq_send(&uart_1_rx_mq, &msg, sizeof(struct rx_msg)); 
 }
 
 ALIGN(RT_ALIGN_SIZE)
 static char thread_uart_recv_stack[512];
 struct rt_thread thread_uart_recv;
-static void rt_thread_entry_uart_recv(void* parameter)
-{
+static void rt_thread_entry_uart_recv(void* parameter) {
   rt_device_t device;
   rt_err_t result = RT_EOK;
-  struct rx_msg msg;  
-  static char uart_rx_buffer[64];
+  struct rx_msg msg;
+  static char uart_rx_buffer[32];
   device = rt_device_find("uart1");
   if (device == RT_NULL) {
     rt_kprintf("Device not found!\r\n");
@@ -63,14 +71,14 @@ static void rt_thread_entry_uart_recv(void* parameter)
     result = rt_device_open(device, RT_DEVICE_OFLAG_RDWR);
   }
   while(1) {
-    result = rt_mq_recv(&rx_mq, &msg, sizeof(struct rx_msg), 50);
+    result = rt_mq_recv(&uart_1_rx_mq, &msg, sizeof(struct rx_msg), 50);
     if (result == -RT_ETIMEOUT) {
-    
+      //no message, meaning that no UART data received
     }
     else if (result == RT_EOK) {
       rt_uint16_t rx_length = 0;
       rx_length = (sizeof(uart_rx_buffer) - 1) > msg.size ?  
-                msg.size : sizeof(uart_rx_buffer) - 1;
+                  msg.size : sizeof(uart_rx_buffer) - 1;
       rx_length = rt_device_read(device, 0, &uart_rx_buffer[0], rx_length);
       uart_rx_buffer[rx_length] = '\0'; 
       rt_device_write(device, 0, &uart_rx_buffer[0], rx_length);
@@ -81,7 +89,7 @@ static void rt_thread_entry_uart_recv(void* parameter)
 int rt_application_init()
 {
   rt_err_t result;   
-  result = rt_mq_init(&rx_mq, "mqt", &msg_pool[0], 128 - sizeof(void*), sizeof(msg_pool), RT_IPC_FLAG_FIFO); 
+  result = rt_mq_init(&uart_1_rx_mq, "mqt", &msg_pool[0], 128 - sizeof(void*), sizeof(msg_pool), RT_IPC_FLAG_FIFO); 
   if (result != RT_EOK) {   
     rt_kprintf("init message queue failed.\n");
   } 
